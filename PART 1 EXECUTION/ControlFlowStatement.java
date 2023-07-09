aea43a68297daa344769c6bcd0002ca4d1b18097
Sarvesh Udapurkar <93316880+sarveshjain2002@users.noreply.github.com>
2023-07-09 21:35:27 +0530

    Add files via upload

---
+++ PART 1 EXECUTION/ControlFlowStatement.java
@@ -0,0 +1,27 @@
+//Decison Making-Control Flow Statement
+//1.if the
+//2.if else
+//3.if else if
+//4.Nested if
+
+class ControlFlowStatement
+{
+public static void main(String args[])
+{
+int a=10, b=15;
+
+if(a>b)
+{
+System.out.println("A is Greater than B");
+}
+else if(a>8)
+{
+System.out.println("Value of A is Greater");
+}
+else
+{
+System.out.println("B is Greater than A");
+}
+
+}
+}
